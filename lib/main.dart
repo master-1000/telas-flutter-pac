78850e7b58cab2defe15cad7a866def1ed593f81
master-1000 <carloseduardoleal10@gmail.com>
2022-12-04 20:07:23 -0300

    upload GitHub

---
+++ lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated4scanningwidget/Generated4ScanningWidget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated4scanningwidget1/Generated4ScanningWidget1.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedrectangle8widget/GeneratedRectangle8Widget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated3inicialwidget/Generated3InicialWidget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated3inicialwidget1/Generated3InicialWidget1.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated3inicialwidget2/Generated3InicialWidget2.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated4scanningwidget2/Generated4ScanningWidget2.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedrectangle18widget/GeneratedRectangle18Widget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedreprovarwidget/GeneratedReprovarWidget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedrectangle19widget/GeneratedRectangle19Widget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedaprovarwidget/GeneratedAprovarWidget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedsalvarwidget/GeneratedSalvarWidget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated3inicialwidget3/Generated3InicialWidget3.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated3inicialwidget4/Generated3InicialWidget4.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated2loginwidget/Generated2LoginWidget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated1carregamentowidget/Generated1CarregamentoWidget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedrectangle9widget4/GeneratedRectangle9Widget4.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedvectorwidget94/GeneratedVectorWidget94.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedemanlisecomocorrdenadorwidget1/GeneratedEmanlisecomocorrdenadorWidget1.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated4scanningwidget3/Generated4ScanningWidget3.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedrectangle10widget2/GeneratedRectangle10Widget2.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedvectorwidget103/GeneratedVectorWidget103.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedaprovadopelocoordenadorwidget/GeneratedAprovadopelocoordenadorWidget.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generated4scanningwidget4/Generated4ScanningWidget4.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedrectangle11widget2/GeneratedRectangle11Widget2.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedvectorwidget112/GeneratedVectorWidget112.dart';
+import 'package:flutterapp/telas_flutter_pacapp/generatedreprovadopelocoordenadorwidget/GeneratedReprovadopelocoordenadorWidget.dart';
+
+void main() {
+  runApp(Telas_Flutter_PACApp());
+}
+
+class Telas_Flutter_PACApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/Generated4ScanningWidget',
+      routes: {
+        '/Generated4ScanningWidget': (context) => Generated4ScanningWidget(),
+        '/Generated4ScanningWidget1': (context) => Generated4ScanningWidget1(),
+        '/GeneratedRectangle8Widget': (context) => GeneratedRectangle8Widget(),
+        '/Generated3InicialWidget': (context) => Generated3InicialWidget(),
+        '/Generated3InicialWidget1': (context) => Generated3InicialWidget1(),
+        '/Generated3InicialWidget2': (context) => Generated3InicialWidget2(),
+        '/Generated4ScanningWidget2': (context) => Generated4ScanningWidget2(),
+        '/GeneratedRectangle18Widget': (context) =>
+            GeneratedRectangle18Widget(),
+        '/GeneratedReprovarWidget': (context) => GeneratedReprovarWidget(),
+        '/GeneratedRectangle19Widget': (context) =>
+            GeneratedRectangle19Widget(),
+        '/GeneratedAprovarWidget': (context) => GeneratedAprovarWidget(),
+        '/GeneratedSalvarWidget': (context) => GeneratedSalvarWidget(),
+        '/Generated3InicialWidget3': (context) => Generated3InicialWidget3(),
+        '/Generated3InicialWidget4': (context) => Generated3InicialWidget4(),
+        '/Generated2LoginWidget': (context) => Generated2LoginWidget(),
+        '/Generated1CarregamentoWidget': (context) =>
+            Generated1CarregamentoWidget(),
+        '/GeneratedRectangle9Widget4': (context) =>
+            GeneratedRectangle9Widget4(),
+        '/GeneratedVectorWidget94': (context) => GeneratedVectorWidget94(),
+        '/GeneratedEmanlisecomocorrdenadorWidget1': (context) =>
+            GeneratedEmanlisecomocorrdenadorWidget1(),
+        '/Generated4ScanningWidget3': (context) => Generated4ScanningWidget3(),
+        '/GeneratedRectangle10Widget2': (context) =>
+            GeneratedRectangle10Widget2(),
+        '/GeneratedVectorWidget103': (context) => GeneratedVectorWidget103(),
+        '/GeneratedAprovadopelocoordenadorWidget': (context) =>
+            GeneratedAprovadopelocoordenadorWidget(),
+        '/Generated4ScanningWidget4': (context) => Generated4ScanningWidget4(),
+        '/GeneratedRectangle11Widget2': (context) =>
+            GeneratedRectangle11Widget2(),
+        '/GeneratedVectorWidget112': (context) => GeneratedVectorWidget112(),
+        '/GeneratedReprovadopelocoordenadorWidget': (context) =>
+            GeneratedReprovadopelocoordenadorWidget(),
+      },
+    );
+  }
+}
